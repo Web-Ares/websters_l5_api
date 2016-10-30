f5f17f20f4aa716948fc48d28988852e685dab32
aleksandrnikolajcuk <deathstars111>
2016-10-30 16:47:54 +0200

    MIddleware for CheckGoogle

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Websters API Development
                 </div>
 
                 <div class="links">
