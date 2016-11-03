e8233590b17ee7aa7d60a584824871d0a4f37f78
aleksandrnikolajcuk <deathstars111>
2016-11-03 14:32:41 +0200

    Websters auth

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,22 +16,15 @@ class Cors
 
     public function handle($request, Closure $next)
     {
-        header('Access-Control-Allow-Origin: *');
-
-        // ALLOW OPTIONS METHOD
-        $headers = [
-            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization'
-        ];
-        if ($request->getMethod() == "OPTIONS") {
-            // The client-side application can set only headers allowed in Access-Control-Allow-Headers
-            return \Response::make('OK', 200, $headers);
-        }
-
-        $response = $next($request);
-        foreach ($headers as $key => $value)
-            $response->header($key, $value);
-        return $response;
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', $_SERVER['HTTP_ORIGIN'])
+            // Depending of your application you can't use '*'
+            // Some security CORS concerns
+            //->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'POST, OPTIONS')
+            ->header('Access-Control-Allow-Credentials', 'true')
+            ->header('Access-Control-Max-Age', '10000')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
     }
 
 
