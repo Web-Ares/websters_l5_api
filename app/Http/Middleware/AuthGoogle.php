4b44d15a6a54c2f93450f243fd5df0bacf9f60de
aleksandrnikolajcuk <deathstars111>
2016-11-04 15:38:42 +0200

    Websters auth

--- app/Http/Middleware/AuthGoogle.php
+++ app/Http/Middleware/AuthGoogle.php
@@ -34,7 +34,7 @@ class AuthGoogle
             $token = $access_array_token['access_token'];
             $refresh = $access_array_token['refresh_token'];
 
-            return response()->json($access_array_token);
+            return response($code);
 
             if(!is_null($token) && !is_null($refresh)){
                 
