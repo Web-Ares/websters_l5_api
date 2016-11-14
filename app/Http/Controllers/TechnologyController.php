0d12a2dc3b5e24d58d8757db16bf1dd8bec74703
aleksandrnikolajcuk <deathstars111>
2016-11-14 10:27:35 +0200

    PUT technologies

--- app/Http/Controllers/TechnologyController.php
+++ app/Http/Controllers/TechnologyController.php
@@ -189,7 +189,7 @@ class TechnologyController extends Controller
 
         if(!is_null($technology)){
 
-            $outputName = Input::file('img')->getClientOriginalName();
+//            $outputName = Input::file('img')->getClientOriginalName();
 
             return response()->json($request->all());
 
